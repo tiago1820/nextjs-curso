be22d12794b48b21c451f8798a61df8635ae6863
tiago <tiago.zdo@gmail.com>
2024-05-25 12:43:10 -0300

    Metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import Navbar from "../components/Navbar";
 
+export const metadata = {
+  title: "Tiago",
+  description: "Esta es la pagina principal de mi tienda",
+  keywords: "tienda, online, ecommerce",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
