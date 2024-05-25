d8fe14e4813f3b7e51e8b234f2e6d36ac29ddd65
tiago <tiago.zdo@gmail.com>
2024-05-25 16:33:46 -0300

    Params

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,11 +1,15 @@
 "use client";
+import Link from "next/link";
+
 // React Client Component
 const PostCard = ({ post }) => {
     return (
         <div>
-            <h3>{post.id}. {post.title}</h3>
+            <Link href={`/posts/${post.id}`}>
+                <h3>{post.id}. {post.title}</h3>
+            </Link>
             <p>{post.body}</p>
-            <button onClick={() => { 
+            <button onClick={() => {
                 alert("Click funcionando!")
             }}>
                 click
