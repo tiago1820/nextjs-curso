1c3a79f3ade7bfdb3b429b0f3e63993e60f182d8
tiago <tiago.zdo@gmail.com>
2024-05-25 13:16:37 -0300

    Fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,22 @@
 import Navbar from "../components/Navbar";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
   title: "Tiago",
   description: "Esta es la pagina principal de mi tienda",
   keywords: "tienda, online, ecommerce",
-}
+};
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  styles: ["italic", "normal"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
