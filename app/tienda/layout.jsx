f777b8b179645e8a07c9faf88e052f7359e53b77
tiago <tiago.zdo@gmail.com>
2024-05-25 11:30:46 -0300

    Layout

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function TiendaLayout({ children }) {
+    return <>
+
+        <nav>
+            <h3>Seccion Tienda</h3>
+
+            <ul>
+                <li>
+                    <Link href="/tienda/categorias">Categorias</Link>
+                </li>
+                <li>
+                    <Link href="/tienda/categorias/computadoras">Computadoras</Link>
+                </li>
+            </ul>
+        </nav>
+
+        {children}</>;
+}
