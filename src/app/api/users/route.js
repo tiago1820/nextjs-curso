17c2e60818bb75b3eba6e1bbb0a56657b5d22d2e
tiago <tiago.zdo@gmail.com>
2024-05-26 18:34:39 -0300

    Environment variable

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
 
+console.log(process.env.TOKEN);
+console.log(process.env.SECRET_KEY);
+
 export async function GET() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await res.json();
+
+    console.log("https://jsonplaceholder.typicode.com/users" + process.env.TOKEN);
 
     return NextResponse.json(data);
 }
