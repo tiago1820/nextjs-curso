09f77d10e2613d513f6492d59297724f0eea62b3
tiago <tiago.zdo@gmail.com>
2024-05-26 15:38:59 -0300

    Route handlers params

---
+++ src/app/api/users/[userId]/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
+    const data = await res.json();
+    return NextResponse.json(data);
+}
